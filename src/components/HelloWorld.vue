61d4357e0cfad54d446de89c910c731b10e30f3b
Ochuko Kevwe <kevweochu@gmail.com>
2019-01-02 04:38:03 +0100

    Design added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,36 +2,40 @@
   <div class="hello">
     <h1>What is Lorem Ipsum??</h1>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    <router-link to="/questions" tag="button">Start</router-link>
+    <router-link to="/questions" class="start" tag="button">Start</router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
-  font-weight: normal;
+  font-weight: bold;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.start {
+  background-color: tomato;
+  height: 50px;
+  width: 80px;
+  border-radius: 10px;
+  margin-top: 20px;
 }
-a {
-  color: #42b983;
+.hello {
+  text-align: center;
+  padding: 30px 200px;
 }
 </style>
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,105 +1,85 @@
 <template>
-  <div>
-    <h1>My view of me</h1>
-    <p>This part says alot about you.</p>
+  <div class="quest">
+    <div class="lines">
+      <h3>My view of me</h3>
+      <p>This part measures the extent to which you have traits to make an employee great.</p>
 
-    <br>
-    <p>Scoring:</p>
-    <p>1 - No this is not me</p>
-    <p>2 - Not Quite Me</p>
-    <p>3 - Yes this is me</p>
-
+      <br>
+      <p>Scoring:</p>
+      <p>1) No this is not me</p>
+      <p>2) Not Quite Me</p>
+      <p>3) Yes this is me</p>
+    </div>
     <div>
       <form>
-        <div>
+        <div class="borders">
           <div v-bind:class="{ color1: this.score1 == '' && this.attempt != 0 }">
-            <span>1.</span>
+            <h4>1.</h4>
             {{questions.questions[0]}}
           </div>
-          <label>
-            <input type="radio" v-model="score1" name="score" value="1" required>
-            1
-          </label>
-          <label>
-            <input type="radio" v-model="score1" name="score" value="2">
-            2
-          </label>
-          <label>
-            <input type="radio" v-model="score1" name="score" value="3">
-            3
-          </label>
+
+          <input id="sc1" type="radio" v-model="score1" name="score" value="1" required>
+          <label for="sc1">1</label>
+
+          <input id="sc2" type="radio" v-model="score1" name="score" value="2">
+          <label for="sc2">2</label>
+
+          <input id="sc3" type="radio" v-model="score1" name="score" value="3">
+          <label for="sc3">3</label>
         </div>
-        <div>
+        <div class="borders">
           <div v-bind:class="{ color1: this.score2 == '' && this.attempt != 0 }">
-            <span>2.</span>
+            <h4>2.</h4>
             {{questions.questions[1]}}
           </div>
-          <label>
-            <input type="radio" v-model="score2" name="score2" value="1" required>
-            1
-          </label>
-          <label>
-            <input type="radio" v-model="score2" name="score2" value="2">
-            2
-          </label>
-          <label>
-            <input type="radio" v-model="score2" name="score2" value="3">
-            3
-          </label>
+
+          <input id="sc21" type="radio" v-model="score2" name="score2" value="1" required>
+          <label for="sc21">1</label>
+          <input id="sc23" type="radio" v-model="score2" name="score2" value="2">
+          <label for="sc23">2</label>
+          <input id="sc22" type="radio" v-model="score2" name="score2" value="3">
+          <label for="sc22">3</label>
         </div>
-        <div>
+        <div class="borders">
           <div v-bind:class="{ color1: this.score3 == '' && this.attempt != 0 }">
-            <span>3.</span>
+            <h4>3.</h4>
             {{questions.questions[2]}}
           </div>
-          <label>
-            <input type="radio" v-model="score3" name="score3" value="1" required>
-            1
-          </label>
-          <label>
-            <input type="radio" v-model="score3" name="score3" value="2">
-            2
-          </label>
-          <label>
-            <input type="radio" v-model="score3" name="score3" value="3">
-            3
-          </label>
+
+          <input id="sc31" type="radio" v-model="score3" name="score3" value="1" required>
+          <label for="sc31">1</label>
+          <input id="sc32" type="radio" v-model="score3" name="score3" value="2">
+          <label for="sc32">2</label>
+          <input id="sc33" type="radio" v-model="score3" name="score3" value="3">
+          <label for="sc33">3</label>
         </div>
-        <div>
+        <div class="borders">
           <div v-bind:class="{ color1: this.score4 == '' && this.attempt != 0 }">
-            <span>4.</span>
+            <h4>4.</h4>
             {{questions.questions[3]}}
           </div>
-          <label>
-            <input type="radio" v-model="score4" name="score4" value="1" required>
-            1
-          </label>
-          <label>
-            <input type="radio" v-model="score4" name="score4" value="2">
-            2
-          </label>
-          <label>
-            <input type="radio" v-model="score4" name="score4" value="3">
-            3
-          </label>
+
+          <input id="sc41" type="radio" v-model="score4" name="score4" value="1" required>
+          <label for="sc41">1</label>
+
+          <input id="sc42" type="radio" v-model="score4" name="score4" value="2">
+          <label for="sc42">2</label>
+
+          <input id="sc43" type="radio" v-model="score4" name="score4" value="3">
+          <label for="sc44">3</label>
         </div>
-        <div>
+        <div class="borders">
           <div v-bind:class="{ color1: this.score5 == '' && this.attempt != 0 }">
-            <span>5.</span>
+            <h4>5.</h4>
             {{questions.questions[4]}}
           </div>
-          <label>
-            <input type="radio" v-model="score5" name="score5" value="1" required>
-            1
-          </label>
-          <label>
-            <input type="radio" v-model="score5" name="score5" value="2">
-            2
-          </label>
-          <label>
-            <input type="radio" v-model="score5" name="score5" value="3">
-            3
-          </label>
+
+          <input id="sc51" type="radio" v-model="score5" name="score5" value="1" required>
+          <label for="sc51">1</label>
+          <input id="sc52" type="radio" v-model="score5" name="score5" value="2">
+          <label for="sc52">2</label>
+          <input id="sc53" type="radio" v-model="score5" name="score5" value="3">
+          <label for="sc53">3</label>
         </div>
         <!-- <ul v-for="(list, index) in questions.questions" :key="index">
         <li>
@@ -112,7 +92,7 @@
         </label>
         </ul>-->
         <!-- <router-link v-if="score !== null" to="/successful" tag="button">Submit</router-link> -->
-        <input type="submit" @click.prevent="validateForm">
+        <input class="start" type="submit" @click.prevent="validateForm">
       </form>
     </div>
   </div>
@@ -167,14 +147,38 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.quest {
+  padding: 0px 400px;
+  text-align: left;
+}
+.lines {
+  line-height: 0.5;
+}
+.borders {
+  border: 0.5px solid black;
+  border-radius: 5px;
+  padding: 20px 5px;
+  margin-bottom: 20px;
+}
+.borders input[type="radio"] {
+  display: none;
+}
+
+.borders label {
+  display: inline-block;
+  margin: 0px 20px;
+  padding: 10px 40px;
+  font-family: Arial;
+  font-size: 16px;
+  border-radius: 4px;
+}
+
+.borders label:hover {
+  background-color: rgb(238, 153, 153);
+}
+.borders input[type="radio"]:checked + label {
+  background-color: rgb(226, 93, 93);
 }
 li {
   list-style: none;
@@ -184,5 +188,12 @@ li {
 }
 .color2 {
   color: white;
+}
+.start {
+  background-color: tomato;
+  height: 50px;
+  width: 80px;
+  border-radius: 10px;
+  margin-top: 20px;
 }
 </style>
